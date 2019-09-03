fd354814c09a6520698e5c5b361769c22d444d15
Heberth Pernia <Heberth@myrakle.com>
2019-09-03 14:56:51 -0600

    seccion carrito de compra

--- scripts/myrakle.js
+++ scripts/myrakle.js
@@ -1,4 +1,30 @@
-function openPanel(){
-	var panel = document.getElementById(cartPanel)
-	panel.style.height = "0px";
+function tooglePanel(x){
+	var panel = document.getElementById(x)
+	if(x == "cartPanel"){
+		if(panel.style.height == "25rem"){
+	        panel.style.height = "0px";
+	        
+	    } else {
+	        panel.style.height = "25rem";
+	        
+	    }
+	}
+	if(x == "searchPanel"){
+		if(panel.style.display == "flex"){
+			document.getElementById("logo").style.display = "flex";
+			document.getElementById("header-bottom-nav").style.display = "flex";
+			panel.style.display = "none";
+
+		}else{
+
+			document.getElementById("logo").style.display = "none";
+			document.getElementById("header-bottom-nav").style.display = "none";
+			panel.style.display = "flex";
+
+		}
+
+		
+		
+
+	}
 }
